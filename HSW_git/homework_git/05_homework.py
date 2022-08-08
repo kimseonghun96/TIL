1f87a457a5656ca4c0fa5396332badcf71b13df2
kimseonghun96 <tjd951753@naver.com>
2022-08-09 00:39:46 +0900

    0808

---
+++ HSW_git/homework_git/05_homework.py
@@ -0,0 +1,6 @@
+#  코드를 고르시오.
+print(str(1))
+print(int('30'))
+print(int(5))
+print(bool('50'))
+# print(int('3.5')) #오류 발생
---
+++ HSW_git/homework_git/06_homework.py
@@ -0,0 +1,7 @@
+n = 5
+m = 9
+for i in range(n):
+    for j in range(m):
+        print('*', end='')
+    print()
+    
