4d2c0f8c50b747b23843559ff60f3a73a132328f
kimseonghun96 <https://github.com/kimseonghun96>
2022-08-17 23:14:46 +0900

    0817

--- TILday/0811/이진탐색.py
+++ TILday/0811/이진탐색.py
@@ -1,28 +1,10 @@
-#아직 덜 완성됨
-
-def binarySearch(N, key):  # N = 검색 대상 배열  key = 찾고자하는 대상
-    start = 1  # 시작과 끝을 정함
-    end = N
-    cnt = 0
-    while start <= end:  # =를 빼먹으면 start == end가 같을경우도 비교하기 때문에
-        middle = (start + end) // 2  # 이진 검색을 하기 위한 중간지점
-        if N[middle] == key:  # 검색 성공
-            return cnt
-        elif N[middle] > key:  # 찾고자 하는 것이 크다면
-            end - middle  # 반대 쪽 지워버림
-        else:
-            start = middle  # 끝을 정했기에 넘어서면 start와 end 위치가 역전됨 실패
-            cnt += 1  # 검색실패
+T=int(input())
+for tc in range(T):
+    N = int(input())
+    # num_sort=map(int, input().split())
+    for num in range(N):
+        num_sort = [i]
+        if num_sort[i] < num:
+            num_sort[i], num_sort[j] = num_sort[j], num_sort[i]
 
 
-T = int(input())
-for tc in range(1, T+1):
-    P, Pa, Pb = map(int, input().split())            #P는 크기고 인덱스 들어갈 수로 변환해서 넣어야됨
-    if binarySearch(P, Pa) < binarySearch(P, Pb):
-        print(f'# {tc} A')
-
-    elif binarySearch(P. PA) > binarySearch(P, Pb):
-        print(f'# {tc} B')
-
-    else:
-        print(f'# {tc} 0')
