ebb3ecdaa9b6d53f59af1abc7189158eb49da088
kimseonghun96 <tjd951753@naver.com>
2022-08-26 00:27:51 +0900

    0826

--- TILday/im 대비/1959_두 개의 숫자열.py
+++ TILday/im 대비/1959_두 개의 숫자열.py
@@ -1,34 +1,37 @@
 import sys
 sys.stdin = open('두개의 숫자열 input.txt')
 
-T = int ( input () )
-for tc in range ( 1, T + 1 ):
-    N, M = map ( int, input ().split () )
-    A = list ( input ().split () )  # 1차원 리스트 만듬
-    B = list ( input ().split () )
-    # print(list_A)
-    max_num = []  # sum_num 들을  받고, 마지막에 솔트 한담에 -1꺼 뽑으면 될 듯?
+# A와 B의 요소를 곱한 값을 하나의 리스트에 모으고 솔트를 사용해
+# 가장 큰 수를 [-1]자리로 옮기고 출력
 
-    for i in range ( N ):
-        sum = 0
-        if len ( A ) == len ( B ):
-            sum += (A[i] * B[i])
-        max_num.append(sum)
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    # print(A)
 
-    for i in range(M-N+1):
-        sum = 0
-        for j in range(len(A)):
-            if len(A) < len(B):
-                sum += A[j]*B[i]
-            max_num.append(sum)
+    if N == M:                      # N과 M이 같을 때
+        max = 0                     # 최대값 변수
+        for i in range(N):          # 작은 범위를 기준으로(같으므로 상관 x)
+            max += A[i] * B[i]      # max에 추가
 
+    else:                               # 반대경우 N과 M이 다를 때
+        if N > M:                       # N이 M보다 클 때
+            sum = [0] * (N - M + 1)     # N - M의 범위를 비교 하기 위해 +1
+            for i in range(len(sum)):
+                for j in range(-1, -M - 1, -1):
+                    sum[i] += A[j - i] * B[j]
         else:
-            sum += A[j]*B[i+j]
-            max_num.append(sum)
+            sum = [0] * (M - N + 1)
+            for i in range(len(sum)):
+                for j in range(-1, -N - 1, -1):
+                    sum[i] += A[j] * B[j - i]
+        # print(sum)
+        sum.sort()
+        max = sum[-1]
 
-
-    max_num = max_num.sort ()
-    print ( f'#{tc} {max_num[-1]}' )
+    print(f'#{tc} {max}')
 
 
 
@@ -48,7 +51,7 @@ for tc in range ( 1, T + 1 ):
 #     N, M = map ( int, input ().split () )
 #     A = list ( input ().split () )  # 1차원 리스트 만듬
 #     B = list ( input ().split () )
-#     # print(A)
+    # print(A)
 #     max_num = []  # sum_num 들을  받고, 마지막에 솔트 한담에 -1꺼 뽑으면 될 듯?
 #
 #     for i in range ( len ( B ) - len ( A ) + 1 ):
