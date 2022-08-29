88a4643cfa619634c71b78694dc993d050cf695f
kimseonghun96 <tjd951753@naver.com>
2022-08-29 18:10:58 +0900

    0829

--- TILday/im 대비/1974_스도쿠 검증.py
+++ TILday/im 대비/1974_스도쿠 검증.py
@@ -54,7 +54,7 @@ for tc in range(1, T+1):
 
 print('-----------------------------------------------------------------------------------')
 import sys
-sys.stdin = open('input2.txt')
+sys.stdin = open('test_input.txt')
 
 T = int(input())
 for tc in range(1, T+1):
--- TILday/im 대비/test.py
+++ TILday/im 대비/test.py
@@ -3,14 +3,18 @@ sys.stdin = open('test_input.txt')
 
 T = int(input())
 for tc in range(1, T+1):
-    arr = [list(map(int, input().split())) for _ in range(9)]
-    # print(arr)
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
-    row_list = []
-    for r in range(3):
-        temp_list = []
-        for c in range(3):
+    answer = 0
+    for r in range(N-M+1):
+        for c in range(N-M+1):
+            value = 0
+            for i in range(M):
+                for j in range(M):
+                    value += arr[r+i][c+j]
+                    if answer < value:
+                        answer = value
 
-
-
+    print(f'#{tc} {answer}')
 
