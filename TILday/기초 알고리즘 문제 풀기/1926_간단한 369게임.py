e26b4c634dbe55e14cb43b4197b2488e3c0717cd
kimseonghun96 <tjd951753@naver.com>
2022-09-01 17:43:43 +0900

    0901

---
+++ TILday/기초 알고리즘 문제 풀기/1926_간단한 369게임.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+arr = []
+for i in range(1, N+1):
+    arr.append(i)
+    if 3 or 6 or 9 in arr[i]:
+        for j in range(1, len(arr)+1):
+            3 or 6 or 9 = '-'
+
+
+    # if arr in 3:
+    # arr = '-'
+    # if i == 3 or i == 6 or i == 9:
+    #     i = '-'
+    # arr.append(i)
+
+
+print(*arr, end='')
