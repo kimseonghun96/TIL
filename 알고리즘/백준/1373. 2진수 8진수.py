4352f25c6b7316e0b072c747311a05b70d407e52
kimseonghun96 <tjd951753@naver.com>
2023-02-14 22:22:14 +0900

    8진수

---
+++ 알고리즘/백준/1373. 2진수 8진수.py
@@ -0,0 +1,13 @@
+import sys
+
+x = sys.stdin.readline()
+ten_number = 0
+answer = ''
+for i in range(len(x)):
+    ten_number += int(x[-1])*(2**i)
+    x = x[:-1]
+
+while ten_number != 0:
+    answer += str(ten_number%8)
+    ten_number = ten_number // 8
+print(oct(int(input(),2))[2:])
