04d538fcef6b591f26d24b51fba89712cfc85f29
kimseonghun96 <tjd951753@naver.com>
2023-09-22 12:21:20 +0900

    algorithm

--- 알고리즘/백준/1978. 소수 찾기.py
+++ 알고리즘/백준/1978. 소수 찾기.py
@@ -1,18 +1,21 @@
 # 특정 숫자 x가 소수인지 판별하는 가장 기본적인 알고리즘
-def primenumber(x):
-    for i in range(2, x): # 2부터 x-1까지의 모든 숫자
-        if x % i == 0:		# 나눠떨어지는게 하나라도 있으면 False
+
+def primenum(x):
+    for i in range(2, x):
+        if x % i == 0:
             return False
-    return True				# 전부 나눠떨어지지 않으면 True
+    return True
 
 
 n = int(input())
-arr = []
 num = list(map(int, input().split()))
+
+cnt = 0
 for i in range(n):
-    if primenumber(num[i]) == True and num[i] != 1:
-        arr.append(primenumber(num[i]))
+    if primenum(num[i]) == True and num[i] != 1:
+        cnt += 1
     else:
         pass
 
-print(len(arr))
+
+print(cnt)
