44a82ba47e1a5ff0f5b9aef01f118d66a4d23dde
kimseonghun96 <tjd951753@naver.com>
2023-10-25 18:31:13 +0900

    algorithm

--- 알고리즘/백준/2164. 카드2.py
+++ 알고리즘/백준/2164. 카드2.py
@@ -1,14 +1,4 @@
-# n = int(input())
-# card = []
-# for i in range(1, n+1):
-#     card.append(i)
-# card.sort()
-#
-# while len(card) != 1:
-#     card.pop(0)
-#     card.append(card.pop(0))
-#
-# print(card[0])
+
 
 from collections import deque
 
