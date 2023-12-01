05e47539dcaf17df54948a507f8e4b1278b54e5e
kimseonghun96 <tjd951753@naver.com>
2023-12-01 20:05:42 +0900

    algorithm

---
+++ 알고리즘/1371. 가장 많은 글자.py
@@ -0,0 +1,19 @@
+# sys.stdin.readline() -> 여러 문자를 입력 받기 위해
+# ord(), chr() 아스키 코드로 변환하는 거
+
+import sys
+
+input = sys.stdin.read
+
+strarr = input().replace("\n","").replace(" ","")
+
+word = [0 for i in range(26)]
+
+
+for s in strarr:
+    if s.islower():
+        word[ord(s) - 97] += 1      # 알파벳을 아스키코드로 변환
+
+for i in range(26):
+    if word[i] == max(word):
+        print(chr(i + 97), end = "")  # 아스키코드에 해당되는 문자로 반환
